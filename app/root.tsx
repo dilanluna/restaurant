c366d0c850ba46b72e43ee8dfcdae9b7cdb882ba
Dylan Luna <me@dilanluna.com>
2022-07-25 10:38:53 -0600

    feat: change page title

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,7 @@ import type { LinksFunction, MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Restaurant',
   viewport: 'width=device-width,initial-scale=1',
 });
 
