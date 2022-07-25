6c446b37f40f9bc7859e3cf9a660b62eb0e4349b
Dylan Luna <me@dilanluna.com>
2022-07-24 21:12:41 -0600

    feat: change default font

--- app/chakra-ui/theme.ts
+++ app/chakra-ui/theme.ts
@@ -4,6 +4,11 @@ const theme = extendTheme({
   colors: {
     'brand-black': '#0E0C0F',
   },
+  fonts: {
+    heading:
+      'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    body: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  },
   styles: {
     global: {
       body: {
