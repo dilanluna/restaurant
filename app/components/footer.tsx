666936a20b8275ef8d885d95a6fd3bb0659e51d8
Dylan Luna <me@dilanluna.com>
2022-07-25 20:08:43 -0600

    feat: add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,67 @@
+import logo from '~/assets/logo_small.png';
+import { Link as RouterLink } from '@remix-run/react';
+import { Box, Center, HStack, Img, Link, Stack, Text } from '@chakra-ui/react';
+import Container from './container';
+
+export default function Footer() {
+  return (
+    <Box
+      py="10"
+      borderTopWidth="1px"
+      borderTopStyle="solid"
+      borderTopColor="white">
+      <Container>
+        <Stack
+          alignItems="center"
+          justifyContent="space-between"
+          direction={{ base: 'column', md: 'row' }}>
+          <HStack>
+            <Link
+              to="/"
+              as={RouterLink}>
+              <Img
+                src={logo}
+                height="64px"
+                alt="48 Cousine"
+              />
+            </Link>
+          </HStack>
+
+          <Stack
+            textAlign="center"
+            justifyContent="space-between"
+            flex={{ base: 'none', lg: '1' }}
+            maxW={{ lg: '620px', xl: '700px' }}
+            spacing={{ base: 6, lg: 10, '2xl': 12 }}
+            direction={{ base: 'column', md: 'row' }}>
+            <Link
+              as={RouterLink}
+              to="/menu">
+              Menu
+            </Link>
+            <Link
+              as={RouterLink}
+              to="/reservation">
+              Reservation
+            </Link>
+            <Link
+              as={RouterLink}
+              to="/about">
+              About
+            </Link>
+            <Link
+              as={RouterLink}
+              to="/contact-us">
+              Contact Us
+            </Link>
+            <Text>Social Media</Text>
+          </Stack>
+        </Stack>
+
+        <Center pt={{ base: '8', md: '5' }}>
+          <Text>&copy; 48 Cuisine Restaurant. All Rights Reserved. 2022</Text>
+        </Center>
+      </Container>
+    </Box>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,19 @@
 import Header from '~/components/header';
+import Footer from '~/components/footer';
 import { Box, HStack } from '@chakra-ui/react';
 import banner from '~/assets/banner_welcome.jpg';
 import HeroSection from '~/components/hero-section';
 
 export default function Index() {
   return (
-    <Box pb="20">
+    <Box pb="10">
       <Header />
       <HeroSection
         cover
         banner={banner}>
         <HStack />
       </HeroSection>
+      <Footer />
     </Box>
   );
 }
