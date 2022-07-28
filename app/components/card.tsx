e8a2cb95cca8f53160d5aadc1100b76f2e2c5fff
Dylan Luna <me@dilanluna.com>
2022-07-27 20:14:38 -0600

    feat: add border prop for card

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,20 +1,24 @@
 import { Box } from '@chakra-ui/react';
 import type { BoxProps } from '@chakra-ui/react';
 
-export type CardProps = BoxProps & { opaque?: boolean };
+export type CardProps = Omit<BoxProps, 'border'> & {
+  opaque?: boolean;
+  border?: 'none' | 'sm' | 'md';
+};
 
 export default function Card({
   children,
+  border = 'md',
   opaque = false,
   ...props
 }: CardProps) {
   return (
     <Box
       {...props}
-      borderWidth="8px"
       borderStyle="solid"
       borderColor="white"
-      bgColor={opaque ? 'brand-black' : 'blackAlpha.800'}>
+      bgColor={opaque ? 'brand-black' : 'blackAlpha.800'}
+      borderWidth={border === 'none' ? '0' : border === 'sm' ? '4px' : '8px'}>
       {children}
     </Box>
   );
