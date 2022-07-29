a6e72d4432dcae6369ba3f355c8d85fa418a2336
Dylan Luna <me@dilanluna.com>
2022-07-28 21:27:49 -0600

    feat: allow hero section getting box props

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -1,17 +1,22 @@
-import type { ReactNode } from 'react';
 import { Box } from '@chakra-ui/react';
+import type { ReactNode } from 'react';
+import type { BoxProps } from '@chakra-ui/react';
+
+type HeroSectionProps = BoxProps & {
+  children: ReactNode;
+  banner?: string;
+  cover?: boolean;
+};
 
 export default function HeroSection({
   children,
   banner,
   cover = false,
-}: {
-  children: ReactNode;
-  banner?: string;
-  cover?: boolean;
-}) {
+  ...props
+}: HeroSectionProps) {
   return (
     <Box
+      {...props}
       maxW="2560px"
       minH={cover ? '100vh' : '640px'}
       {...(banner && {
